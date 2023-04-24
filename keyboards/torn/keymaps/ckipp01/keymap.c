01a7cdec0c4ddf09043f2151239c365554e99de4
Chris Kipp <ckipp@pm.me>
2023-04-24 15:45:10 +0200

    initial mappings

--- keyboards/torn/keymaps/ckipp01/keymap.c
+++ keyboards/torn/keymaps/ckipp01/keymap.c
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * | SHIFT|   Z  |   X  |   C  |   V  |   B  |    |   N  |   M  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
- *               | LT   |  Cmd | Ent  | Ctrl |    |Bksp  |Space | Ctrl | Play |
+ *               | LT   |  Cmd | Ent  | Ctrl |    |Bksp  |Space | Ctrl | Cmd |
  *               `---------------------------'    `---------------------------'
  */
 [_QWERTY] = LAYOUT_split_3x6_4(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LEFT_SHIFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC ,
-            TG(_SECONDARY), KC_LEFT_GUI, KC_ENTER,  KC_LCTL,    KC_BSPC,   KC_SPC,   KC_RCTL, KC_MEDIA_PLAY_PAUSE
+            TG(_SECONDARY), KC_LEFT_GUI, KC_ENTER,  KC_LCTL,    KC_BSPC,  LT(_SECONDARY, KC_SPC),   KC_RIGHT_GUI, KC_MEDIA_PLAY_PAUSE
 ),
 
 /* SECONDARY
